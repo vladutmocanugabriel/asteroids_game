58c6d95f01c33979682115d794ac04f0e3adc8ab
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2024-10-13 20:36:21 +0300

    added a new resource in gitignore and 2 more py files regarding sprites for game elements. Also added some logic for game fps tracking.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import pygame
+from constants import *
 
 def main():
     print('Starting asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
+    FPS_clock = pygame.time.Clock()
+    dt = 0
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return    
+        pygame.Surface.fill(screen, 'black')
+        pygame.display.flip()
+        dt = FPS_clock.tick(60) / 1000
+        
 
 
 
